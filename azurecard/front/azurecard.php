7e91d178d0958954cd47191e7674b8e6279ba3e5
infrati.dev <infrati@infrati.dev>
2022-02-03 13:44:54 -0300

    feat:Adding Custom.Sistema field and error return in curl

--- azurecard/front/azurecard.php
+++ azurecard/front/azurecard.php
@@ -1,16 +1,13 @@
 <?php
 
-
-
 define('GLPI_ROOT', '../../..');
 include(GLPI_ROOT . '/inc/includes.php');
-
-
 
 $Ticket = new Ticket();
 $Ticket->getFromDB($_POST['id']);
 $name = Html::Clean($Ticket->fields['name']);
 $idTicket = Html::Clean($Ticket->fields['id']);
+
 $name = str_replace('"', "", $name);
 $name = str_replace("'", "", $name);
 $name =  str_replace("/", "-", $name);
@@ -19,18 +16,16 @@ $description = str_replace("/", "-", $description);
 $description = str_replace('"', "",  $description);
 $description = str_replace("'", "",  $description);
 
-
-
-
-$content = Html::Clean($Ticket->fields['content']);
-$AreaPath = "Project\\\Area";
-$LinkGLPI = "http://domain.local/front/ticket.form.php?id=".$idTicket ;
+$AreaPath = "X\\\GLPi";
+$URLGLPI = "http://glpi.local/front/ticket.form.php?id=".$idTicket ;
+$LinkGLPI = "<a target='_blank' href='http://glpi.local/front/ticket.form.php?id=$idTicket'>Acesse Aqui!!!</a>" ;
+$CSistema = "GLPI";
 
  /*
  * POST via curl para criação do backlog com os campos preenchidos do System.Title e System.Description 
  **/
 
-$UserStory = "curl -u {{PAT}} --location -X POST 'https://dev.azure.com/{{ORG}}/{{Project}}/_apis/wit/workitems/\$User%20Story?api-version=6.0' -H 'Content-Type:application/json-patch+json' -d '[ { \"op\":\"add\", \"path\":\"/fields/System.Title\", \"value\":\"". $name  ."\" }, { \"op\":\"add\", \"path\":\"/fields/System.Description\", \"value\":\" ".$content." \" }  , { \"op\":\"add\", \"path\":\"/fields/LinkGLPI\", \"value\": \"". $LinkGLPI ."\" }  , { \"op\":\"add\", \"path\":\"/fields/System.AreaPath\", \"value\": \"". $AreaPath ."\" }  ]'"; 
+$UserStory = "curl --silent --show-error --fail -u {{PAT}} --location -X POST 'https://dev.azure.com/{{ORG}}/{{Project}}/_apis/wit/workitems/\$User%20Story?api-version=6.0' -H 'Content-Type:application/json-patch+json' -d '[ { \"op\":\"add\", \"path\":\"/fields/System.Title\", \"value\":\"". $name  ."\" }, { \"op\":\"add\", \"path\":\"/fields/System.Description\", \"value\":\" ".$description." => Link Chamado no GLPI:  ".$LinkGLPI."   \" }, { \"op\":\"add\", \"path\":\"/fields/Custom.Sistema\", \"value\":\"". $CSistema  ."\" } , { \"op\":\"add\", \"path\":\"/fields/LinkGLPI\", \"value\": \"". $URLGLPI ."\" }  , { \"op\":\"add\", \"path\":\"/fields/System.AreaPath\", \"value\": \"". $AreaPath ."\" }  ]'";
 
 exec($UserStory,$out,$ret_var);
 
@@ -44,8 +39,4 @@ else{
 
 $url = explode("?", $_SERVER['HTTP_REFERER']);
 Html::redirect($url[0] . "?id=" . $_POST['id']);
-
-
-
-
 
